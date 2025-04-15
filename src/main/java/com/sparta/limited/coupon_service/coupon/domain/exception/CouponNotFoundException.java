9b183116c88aa94db865a14d0307043545c28673
Imnotcoderdude <topsolo7@naver.com>
2025-04-16 01:06:02 +0900

    ✨ feat [#8] : 쿠폰 조회 실패 예외 클래스 추가  - 어떤 쿠폰을 찾을 수 없는지 알기 위해 쿠폰 ID 메세지에 추가

---
+++ src/main/java/com/sparta/limited/coupon_service/coupon/domain/exception/CouponNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sparta.limited.coupon_service.coupon.domain.exception;
+
+import com.sparta.limited.common_module.exception.BusinessException;
+import com.sparta.limited.common_module.exception.ErrorCode;
+import java.util.UUID;
+
+public class CouponNotFoundException extends BusinessException {
+
+    public CouponNotFoundException(UUID couponId) {
+        super(ErrorCode.RESOURCES_NOT_FOUND, "해당 쿠폰을 찾을 수 없습니다. couponId : " + couponId);
+    }
+}
