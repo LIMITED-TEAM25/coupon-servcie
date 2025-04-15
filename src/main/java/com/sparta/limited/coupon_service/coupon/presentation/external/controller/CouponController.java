969f23636857338dee71246e2bb2c002784a1c27
Imnotcoderdude <topsolo7@naver.com>
2025-04-15 16:39:00 +0900

    ✨ feat [#1] : 쿠폰 컨트롤러 생성 - 쿠폰 생성 API 추가

---
+++ src/main/java/com/sparta/limited/coupon_service/coupon/presentation/external/controller/CouponController.java
@@ -0,0 +1,28 @@
+package com.sparta.limited.coupon_service.coupon.presentation.external.controller;
+
+import com.sparta.limited.coupon_service.coupon.application.dto.request.CouponCreateRequest;
+import com.sparta.limited.coupon_service.coupon.application.dto.response.CouponCreateResponse;
+import com.sparta.limited.coupon_service.coupon.application.service.CouponService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/coupons")
+public class CouponController {
+
+    private final CouponService couponService;
+
+    @PostMapping()
+    public ResponseEntity<CouponCreateResponse> createCoupon(
+        @RequestBody CouponCreateRequest request
+    ) {
+        CouponCreateResponse response = couponService.createCoupon(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
