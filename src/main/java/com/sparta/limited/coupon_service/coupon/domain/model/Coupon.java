bab3efc9f1980bd3e150acb35143eab0a8cff75d
Imnotcoderdude <topsolo7@naver.com>
2025-04-16 00:57:09 +0900

    ✨ feat [#8] : coupon 엔티티에 재고 수량 필드 추가  - 재고 감소 메서드 추가

--- src/main/java/com/sparta/limited/coupon_service/coupon/domain/model/Coupon.java
+++ src/main/java/com/sparta/limited/coupon_service/coupon/domain/model/Coupon.java
@@ -1,6 +1,7 @@
 package com.sparta.limited.coupon_service.coupon.domain.model;
 
 import com.sparta.limited.common_module.common.BaseEntity;
+import com.sparta.limited.coupon_service.coupon.domain.exception.CouponOutOfStockException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,11 +11,11 @@ import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "p_coupon")
 public class Coupon extends BaseEntity {
 
@@ -31,23 +32,36 @@ public class Coupon extends BaseEntity {
     @Column(name = "status", nullable = false)
     private CouponStatus status;
 
+    @Column(name = "quantity", nullable = false)
+    private Long quantity;
+
     private Coupon(
         String name,
-        Integer discountRate
+        Integer discountRate,
+        Long quantity
     ) {
         this.name = name;
         this.discountRate = discountRate;
+        this.quantity = quantity;
         this.status = CouponStatus.ACTIVE;
     }
 
     public static Coupon of(
         String name,
-        Integer discountRate
+        Integer discountRate,
+        Long quantity
     ) {
         return new Coupon(
             name,
-            discountRate
+            discountRate,
+            quantity
         );
     }
 
+    public void decreaseQuantity() {
+        if (quantity <= 0) {
+            throw new CouponOutOfStockException(id);
+        }
+        this.quantity--;
+    }
 }
