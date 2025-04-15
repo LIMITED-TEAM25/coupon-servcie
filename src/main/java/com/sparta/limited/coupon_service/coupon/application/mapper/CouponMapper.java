33e12cfa0ee911a62fc123ad0089d4fffb6ef885
Imnotcoderdude <topsolo7@naver.com>
2025-04-15 16:24:39 +0900

    ✨ feat [#1] : 쿠폰 mapper 클래스 생성 - Entity to DTO, DTO to Entity 정적 메서드 작성

---
+++ src/main/java/com/sparta/limited/coupon_service/coupon/application/mapper/CouponMapper.java
@@ -0,0 +1,28 @@
+package com.sparta.limited.coupon_service.coupon.application.mapper;
+
+import com.sparta.limited.coupon_service.coupon.application.dto.request.CouponCreateRequest;
+import com.sparta.limited.coupon_service.coupon.application.dto.response.CouponCreateResponse;
+import com.sparta.limited.coupon_service.coupon.domain.model.Coupon;
+
+public class CouponMapper {
+
+    public static Coupon toEntity(
+        CouponCreateRequest request
+    ) {
+        return Coupon.of(
+            request.getName(),
+            request.getDiscountRate()
+        );
+    }
+
+    public static CouponCreateResponse toCreateResponse(
+        Coupon coupon
+    ) {
+        return CouponCreateResponse.of(
+            coupon.getId(),
+            coupon.getName(),
+            coupon.getDiscountRate(),
+            coupon.getStatus()
+        );
+    }
+}
