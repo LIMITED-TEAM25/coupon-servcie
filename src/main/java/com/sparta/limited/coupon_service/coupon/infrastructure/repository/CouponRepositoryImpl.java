5efb92dfcf959f258b31dced02c80d901379b20e
Imnotcoderdude <topsolo7@naver.com>
2025-04-16 01:01:40 +0900

    ✨ feat [#8] : CouponRepositoryImpl 쿠폰 ID 기반 조회 메서드 오버라이드  - findById(couponId) 메서드 오버라이드 하여 JPA repository 연결

--- src/main/java/com/sparta/limited/coupon_service/coupon/infrastructure/repository/CouponRepositoryImpl.java
+++ src/main/java/com/sparta/limited/coupon_service/coupon/infrastructure/repository/CouponRepositoryImpl.java
@@ -1,8 +1,10 @@
 package com.sparta.limited.coupon_service.coupon.infrastructure.repository;
 
+import com.sparta.limited.coupon_service.coupon.domain.exception.CouponNotFoundException;
 import com.sparta.limited.coupon_service.coupon.domain.model.Coupon;
 import com.sparta.limited.coupon_service.coupon.domain.repository.CouponRepository;
 import com.sparta.limited.coupon_service.coupon.infrastructure.persistence.JpaCouponRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +17,11 @@ public class CouponRepositoryImpl implements CouponRepository {
     @Override
     public void save(Coupon coupon) {
         jpaCouponRepository.save(coupon);
+    }
+
+    @Override
+    public Coupon findById(UUID couponId) {
+        return jpaCouponRepository.findById(couponId)
+            .orElseThrow(() -> new CouponNotFoundException(couponId));
     }
 }
