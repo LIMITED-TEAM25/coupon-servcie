f706db98b2b751387c75b72570640ac05fc0a669
Imnotcoderdude <topsolo7@naver.com>
2025-04-15 16:22:51 +0900

    ✨ feat [#1] : 쿠폰 저장 Repository 메서드 작성 - JpaRepsitory의 save 메서드 사용

--- src/main/java/com/sparta/limited/coupon_service/coupon/infrastructure/repository/CouponRepositoryImpl.java
+++ src/main/java/com/sparta/limited/coupon_service/coupon/infrastructure/repository/CouponRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.sparta.limited.coupon_service.coupon.infrastructure.repository;
 
+import com.sparta.limited.coupon_service.coupon.domain.model.Coupon;
 import com.sparta.limited.coupon_service.coupon.domain.repository.CouponRepository;
 import com.sparta.limited.coupon_service.coupon.infrastructure.persistence.JpaCouponRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +12,8 @@ public class CouponRepositoryImpl implements CouponRepository {
 
     private final JpaCouponRepository jpaCouponRepository;
 
+    @Override
+    public void save(Coupon coupon) {
+        jpaCouponRepository.save(coupon);
+    }
 }
