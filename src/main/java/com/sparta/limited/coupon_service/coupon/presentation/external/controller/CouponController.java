2a80fe91228b094b7e21ef8ff12e958dabdca9ef
Imnotcoderdude <topsolo7@naver.com>
2025-04-16 14:32:31 +0900

    ✨ feat [#6] : 쿠폰 단일 조회 컨트롤러 추가 - 쿠폰 단일 조회 API의 엔드포인트를 받는 컨트롤러 추가

--- src/main/java/com/sparta/limited/coupon_service/coupon/presentation/external/controller/CouponController.java
+++ src/main/java/com/sparta/limited/coupon_service/coupon/presentation/external/controller/CouponController.java
@@ -3,10 +3,14 @@ package com.sparta.limited.coupon_service.coupon.presentation.external.controlle
 import com.sparta.limited.common_module.common.aop.RoleCheck;
 import com.sparta.limited.coupon_service.coupon.application.dto.request.CouponCreateRequest;
 import com.sparta.limited.coupon_service.coupon.application.dto.response.CouponCreateResponse;
+import com.sparta.limited.coupon_service.coupon.application.dto.response.CouponReadOneResponse;
 import com.sparta.limited.coupon_service.coupon.application.service.CouponService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +24,20 @@ public class CouponController {
     private final CouponService couponService;
 
     @RoleCheck("ROLE_ADMIN")
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<CouponCreateResponse> createCoupon(
         @RequestBody CouponCreateRequest request
     ) {
         CouponCreateResponse response = couponService.createCoupon(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @GetMapping("/{couponId}")
+    public ResponseEntity<CouponReadOneResponse> getOneCoupon(
+        @PathVariable(name = "couponId") UUID couponId
+    ) {
+        CouponReadOneResponse response = couponService.getCoupon(couponId);
+        return ResponseEntity.ok(response);
+    }
+
 }
