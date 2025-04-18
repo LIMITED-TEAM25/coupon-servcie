84f2384d0e80ce0b9f214647b9e0161fd524a42b
Imnotcoderdude <topsolo7@naver.com>
2025-04-18 10:28:18 +0900

    ✨ feat [#16] : Request DTO 클래스 정적 팩토리 메서드 추가 - 테스트시 해당 Request DTO를 정적 팩토리 메서드를 사용하여 생성후 요청 테스트 진행하기 위해 추가

--- src/main/java/com/sparta/limited/coupon_service/coupon/application/dto/request/CouponCreateRequest.java
+++ src/main/java/com/sparta/limited/coupon_service/coupon/application/dto/request/CouponCreateRequest.java
@@ -5,8 +5,29 @@ import lombok.Getter;
 @Getter
 public class CouponCreateRequest {
 
-    private String name;
-    private Integer discountRate;
-    private Long quantity;
+    private final String name;
+    private final Integer discountRate;
+    private final Long quantity;
 
+    private CouponCreateRequest(
+        String name,
+        Integer discountRate,
+        Long quantity
+    ) {
+        this.name = name;
+        this.discountRate = discountRate;
+        this.quantity = quantity;
+    }
+
+    public static CouponCreateRequest of(
+        String name,
+        Integer discountRate,
+        Long quantity
+    ) {
+        return new CouponCreateRequest(
+            name,
+            discountRate,
+            quantity
+        );
+    }
 }
