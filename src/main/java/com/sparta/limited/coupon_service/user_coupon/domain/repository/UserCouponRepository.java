6adba7072e1d5c08f4abcea3f5485e5218b6b1be
Imnotcoderdude <topsolo7@naver.com>
2025-04-24 18:31:40 +0900

    🔥 del [#31] : 인덱스 적용으로 인한 검증 기능 삭제 - 검증용 데이터베이스 조회 메서드 삭제 - 검증 클래스 삭제

--- src/main/java/com/sparta/limited/coupon_service/user_coupon/domain/repository/UserCouponRepository.java
+++ src/main/java/com/sparta/limited/coupon_service/user_coupon/domain/repository/UserCouponRepository.java
@@ -1,11 +1,8 @@
 package com.sparta.limited.coupon_service.user_coupon.domain.repository;
 
 import com.sparta.limited.coupon_service.user_coupon.domain.model.UserCoupon;
-import java.util.UUID;
 
 public interface UserCouponRepository {
 
     void save(UserCoupon userCoupon);
-
-    boolean existsByUserIdAndCouponId(Long userId, UUID couponId);
 }
--- src/main/java/com/sparta/limited/coupon_service/user_coupon/infrastructure/repository/UserCouponRepositoryImpl.java
+++ src/main/java/com/sparta/limited/coupon_service/user_coupon/infrastructure/repository/UserCouponRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.sparta.limited.coupon_service.user_coupon.infrastructure.repository;
 import com.sparta.limited.coupon_service.user_coupon.domain.model.UserCoupon;
 import com.sparta.limited.coupon_service.user_coupon.domain.repository.UserCouponRepository;
 import com.sparta.limited.coupon_service.user_coupon.infrastructure.persistence.JpaUserCouponRepository;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -16,10 +15,5 @@ public class UserCouponRepositoryImpl implements UserCouponRepository {
     @Override
     public void save(UserCoupon userCoupon) {
         jpaUserCouponRepository.save(userCoupon);
-    }
-
-    @Override
-    public boolean existsByUserIdAndCouponId(Long userId, UUID couponId) {
-        return jpaUserCouponRepository.existsByUserIdAndCouponId(userId, couponId);
     }
 }
