54ea7510131d480e342fb86865a6614d5a8fa397
Imnotcoderdude <topsolo7@naver.com>
2025-04-28 20:35:25 +0900

    ✨ feat [#35] : 쿠폰 발급 메서드에 레디스 적용 - 레디스에서 선제적으로 재고감소, 중복 사용자 처리를 진행하여 캐싱된 값을 이용하여 빠르게 처리 - 하지만 테스트 결과 데이터베이스 커넥션은 그대로 남아있기 때문에 속도면에서 향상을 보이지는 않음 - 데이터베이스 커넥션을 끊고 이벤트 드리븐을 적용하는 것이 필요할 것으로 예상된다.

--- src/main/java/com/sparta/limited/coupon_service/user_coupon/application/service/UserCouponService.java
+++ src/main/java/com/sparta/limited/coupon_service/user_coupon/application/service/UserCouponService.java
@@ -3,12 +3,10 @@ package com.sparta.limited.coupon_service.user_coupon.application.service;
 import com.sparta.limited.coupon_service.user_coupon.application.dto.response.UserCouponCreateResponse;
 import com.sparta.limited.coupon_service.user_coupon.application.mapper.UserCouponMapper;
 import com.sparta.limited.coupon_service.user_coupon.application.service.coupon.CouponFacade;
-import com.sparta.limited.coupon_service.user_coupon.domain.exception.UserCouponDuplicatedException;
 import com.sparta.limited.coupon_service.user_coupon.domain.model.UserCoupon;
 import com.sparta.limited.coupon_service.user_coupon.domain.repository.UserCouponRepository;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,18 +16,17 @@ public class UserCouponService {
 
     private final UserCouponRepository userCouponRepository;
     private final CouponFacade couponFacade;
+    private final RedisCouponIssueService redisCouponIssueService;
 
     @Transactional
     public UserCouponCreateResponse creatUserCoupon(
         UUID couponId,
         Long userId
     ) {
+        redisCouponIssueService.quantityAndDuplicate(couponId, userId);
+
         UserCoupon userCoupon = UserCouponMapper.toEntity(couponId, userId);
-        try {
-            userCouponRepository.save(userCoupon);
-        } catch (DataIntegrityViolationException e) {
-            throw new UserCouponDuplicatedException(userId, couponId);
-        }
+        userCouponRepository.save(userCoupon);
 
         couponFacade.decreaseQuantity(couponId);
 
