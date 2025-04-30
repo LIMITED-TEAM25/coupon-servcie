98a99ecaeca508f30bfd408f772d9f4d4e7692f0
Imnotcoderdude <topsolo7@naver.com>
2025-04-30 15:22:24 +0900

    🔥 del [#37] : 분리한 user-coupon 파일 제거

--- src/main/java/com/sparta/limited/coupon_service/user_coupon/infrastructure/persistence/JpaUserCouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.limited.coupon_service.user_coupon.infrastructure.persistence;
-
-import com.sparta.limited.coupon_service.user_coupon.domain.model.UserCoupon;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaUserCouponRepository extends JpaRepository<UserCoupon, UUID> {
-
-}
